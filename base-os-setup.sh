176edf3596aba53ba72ad18f464ab85c67a893d1
Benny Li <dev@benny-li.de>
2019-01-18 10:23:40 +0100

    Optional network setup in base os setup script

--- base-os-setup.sh
+++ base-os-setup.sh
@@ -13,9 +13,6 @@ trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 # Load the key for your country
 loadkeys de-latin1-nodeadkeys
 
-# Setup clock
-timedatectl set-ntp true
-
 
 #####  Get some user input for variable data  ##### {{{1
 
@@ -45,19 +42,29 @@ exec 2> >(tee "stderr.log")
 
 
 #####  Internet connectivity  ##### {{{1
-# Lets connect to the internet
-wifi=$(dialog --stdout --menu "Do you want to connect via wifi or ethernet?" 0 0 0 0 ethernet 1 wifi) || exit 1
-if [ "$wifi" == "1" ]; then
-  wifi-menu
-else
-  interfaces=$(ip -o link | awk '{ gsub(":","",$1); gsub(":","",$2); print $2 " " $1 }')
-  interface=$(dialog --stdout --menu "Select network interface" 0 0 0 $interfaces)
-  dhcpcd $interface
-fi
 
 # Check connectivity
 ping -c 1 8.8.8.8 >> /dev/null
-[[ "$?" == "0" ]] || ( echo "No network connection! Please try again..."; exit 1; )
+if [[ "$?" == "0" ]]; then 
+  echo "Already connected to the internet."
+else
+  # Lets connect to the internet
+  wifi=$(dialog --stdout --menu "Do you want to connect via wifi or ethernet?" 0 0 0 0 ethernet 1 wifi) || exit 1
+  if [ "$wifi" == "1" ]; then
+    wifi-menu
+  else
+    interfaces=$(ip -o link | awk '{ gsub(":","",$1); gsub(":","",$2); print $2 " " $1 }')
+    interface=$(dialog --stdout --menu "Select network interface" 0 0 0 $interfaces)
+    dhcpcd $interface
+  fi
+  
+  # Check connectivity
+  ping -c 1 8.8.8.8 >> /dev/null
+  [[ "$?" == "0" ]] || ( echo "No network connection! Please try again..."; exit 1; )
+fi
+
+# Setup clock
+timedatectl set-ntp true
 
 
 #####  Partitioning  ##### {{{1
